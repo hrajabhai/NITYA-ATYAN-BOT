828f689ff7983530e0bf5690295cfd62ab28183d
Mafiyarandhaha <parsuxrand@gmail.com>
2024-12-20 13:33:40 +0530

    Update 0_ARYAN-FRIEND.js

--- Priyansh/commands/0_ARYAN-FRIEND.js
+++ Priyansh/commands/0_ARYAN-FRIEND.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "Friend",
   version: "7.3.1",
   hasPermssion: 0,
-  credits: "ARYAN", 
+  credits: "AMAN", 
   description: "Get Pair From Mention",
   commandCategory: "png",
   usages: "[@mention]",
