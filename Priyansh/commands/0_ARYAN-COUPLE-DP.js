f9ce838045d56c729869e92f9ef7850f2df151fd
Mafiyarandhaha <parsuxrand@gmail.com>
2024-12-20 13:20:06 +0530

    Update 0_ARYAN-COUPLE-DP.js

--- Priyansh/commands/0_ARYAN-COUPLE-DP.js
+++ Priyansh/commands/0_ARYAN-COUPLE-DP.js
@@ -2,9 +2,9 @@ module.exports.config = {
   name: "couple dp",
   version: "1.0.0",
   hasPermssion: 0,
-  credits: "ARYAN",
+  credits: "AMAN",
   description: "Couple Dp photos",
-  commandCategory: "THIS BOT IS MADE BY ARYAN",
+  commandCategory: "THIS BOT IS MADE BY AMAN",
   usages: "cpl dp",
   cooldowns: 2,
   dependencies: {
